2db5474d61a3b04df4d07c7524f06c38684eeb57
Adam Lovastyik <adam@lovastyik.hu>
2016-08-04 15:45:18 +0200

    Profile view/edit wip

---
+++ Games420/Profile/FTProfileMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FTProfileMainViewController.swift
+//  Games420
+//
+//  Created by Adam Lovastyik on 2016. 08. 04..
+//  Copyright © 2016. ScreamingBox. All rights reserved.
+//
+
+import UIKit
+
+class FTProfileMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
